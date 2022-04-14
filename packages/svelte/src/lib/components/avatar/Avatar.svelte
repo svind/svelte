0d3a752b2c2ee35b40e8b61fad784558a69534bd
TheHadiAhmadi <thehadiahmadi@gmail.com>
2022-04-15 01:12:22 +0430

    svelte - add avatar component

---
+++ packages/svelte/src/lib/components/avatar/Avatar.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+import { Size } from "$lib/types";
+import clsx from "clsx";
+
+
+    export let src = undefined;
+
+    export let initial = undefined
+
+    export let size: Size = undefined;
+
+
+    $: classes = clsx('avatar', {
+        [`avatar-${size}`]: size
+    }
+</script>
+<span class={classes} style="background-image: url({src})">
+    {initial}
+</span>
