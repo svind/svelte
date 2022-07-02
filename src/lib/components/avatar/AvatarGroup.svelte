493a80c5d3be55fd142b847ee66ba5540151f29e
TheHadiAhmadi <thehadiahmadi@gmail.com>
2022-07-02 21:57:34 +0430

    add AvatarGroup svelte componetn

---
+++ src/lib/components/avatar/AvatarGroup.svelte
@@ -0,0 +1,15 @@
+<script>
+import clsx from "clsx";
+
+
+    let className = ''
+    export {className as class}
+
+    $: classes = clsx('avatar-group', {
+        'avatar-group-stacked': stacked,
+    }, className)
+    
+</script>
+<div class={classes}>
+    <slot/>
+</div>
